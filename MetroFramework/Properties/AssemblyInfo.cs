7f811151237665b6c9edbd1fe69becece5c11bbc
Phil Martin <phil@bitplex.com.au>
2017-10-29 13:23:40 +1000

    Updated version number

--- MetroFramework/Properties/AssemblyInfo.cs
+++ MetroFramework/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Security;
 internal static class MetroFrameworkAssembly
 {
     internal const string Title = "MetroFramework.dll";
-    internal const string Version = "1.4.0.0";
+    internal const string Version = "1.4.1.0";
     internal const string Description = "Metro UI Framework for .NET WinForms";
     internal const string Copyright = "Copyright \x00a9 2011 Sven Walter, Dennis Magno.  All rights reserved.";
     internal const string Company = "Sven Walter, Dennis Magno";
